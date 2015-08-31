26037771d6dd39aa9ae08c88bd45d9ce3d20200d
jeslyvarghese <jesly.varghese@gmail.com>
2015-08-31 19:30:02 -0300

    added dispatcher

--- src/toiler/dispatcher/dispatcher.go
+++ src/toiler/dispatcher/dispatcher.go
@@ -1,13 +1,15 @@
 package dispatcher
 
 import (
-	"github.com/jeslyvarghese/liza/src/toiler/worker"
 	"github.com/jeslyvarghese/liza/src/toiler/request"
+	"github.com/jeslyvarghese/liza/src/toiler/worker"
 )
 
 var WorkerQueue chan chan request.Request
 
 func StartDispatcher(nworkers int) {
 	WorkerQueue = make(chan chan request.Request, nworkers)
-	for
+	for i := 0; i < nworkers; i++ {
+		workers := worker.New()
+	}
 }
