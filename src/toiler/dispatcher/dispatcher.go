8873d5a3cc06c2a3064517cda95bdeae9f44f2d1
jeslyvarghese <jesly.varghese@gmail.com>
2015-08-31 22:55:27 -0300

    added dispatcher

--- src/toiler/dispatcher/dispatcher.go
+++ src/toiler/dispatcher/dispatcher.go
@@ -10,6 +10,18 @@ var WorkerQueue chan chan request.Request
 func StartDispatcher(nworkers int) {
 	WorkerQueue = make(chan chan request.Request, nworkers)
 	for i := 0; i < nworkers; i++ {
-		workers := worker.New()
+		worker := worker.New(i, WorkerQueue)
+		worker.Start(p)
 	}
+	go func() {
+		for {
+			select {
+			case work := <-WorkerQueue:
+				go func() {
+					worker := <-WorkerQueue
+					worker <- work
+				}()
+			}
+		}
+	}()
 }
