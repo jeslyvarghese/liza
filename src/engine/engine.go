18590a0a3539189c52826fee30850877c0690363
jesly <jesly.varghese@gmail.com>
2015-11-03 13:03:47 -0300

    updated rackspace upload pathname

--- src/engine/engine.go
+++ src/engine/engine.go
@@ -62,7 +62,7 @@ func ResizeImage(imagePath, imageURL string) (string, bool) {
 
 func UploadImage(imagePath, imageURL string, callback rackspace.UploadCallback) {
 	u, err := url.Parse(imageURL)
-	fileName := u.host+u.path
+	fileName := u.Host+u.Path
 	log.Println("Rackspace filepath:", fileName)
 	cdnURL := "https://03188cc7126169c646ce-4ec321cd871e45e74b11708f248e0363.ssl.cf1.rackcdn.com/"
 	containerName := "merlin"
